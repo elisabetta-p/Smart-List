03697cb3e07a0eeb2072c7d2d226e6ee5f2eecce
elisabetta-p <elisabetta-p@users.noreply.github.com>
2020-08-01 17:04:41 +0000

    deploy: b83ee066e1cc364e61adce32839955b9b07d00f7

---
+++ precache-manifest.30ba53ed2f9600509dec07fa1c80d630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac48c34f072809a1799315a4af9977b",
+    "url": "/Smart-List/index.html"
+  },
+  {
+    "revision": "48b5f90137890d08a677",
+    "url": "/Smart-List/static/css/main.215879b1.chunk.css"
+  },
+  {
+    "revision": "2aabdc5710494679b860",
+    "url": "/Smart-List/static/js/2.8c6998b7.chunk.js"
+  },
+  {
+    "revision": "10ac0cbdb7c9f9b1884f536c41480291",
+    "url": "/Smart-List/static/js/2.8c6998b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b5f90137890d08a677",
+    "url": "/Smart-List/static/js/main.ab1e49a6.chunk.js"
+  },
+  {
+    "revision": "596fa89958bcbb9c2ae0",
+    "url": "/Smart-List/static/js/runtime-main.293aa54d.js"
+  }
+]);
