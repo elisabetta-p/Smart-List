597efe096a6e447146e45547e14a5f0a43064fdf
Elisabetta <elisabetta.piombin@gmail.com>
2020-08-06 12:08:14 +0200

    message and picture to select list

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import ListContainer from "./ListContainer";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getCategories,
   loadShoppingLists,
@@ -11,6 +11,7 @@ import {
   cleanLists,
 } from "../../Redux";
 import MenuLists from "./MenuLists";
+import { ReactComponent as Image } from "../../img/undraw_conceptual_idea_xw7k.svg";
 
 const Profile = (_: void): React$Element<*> => {
   const dispatch = useDispatch();
@@ -23,12 +24,55 @@ const Profile = (_: void): React$Element<*> => {
     };
   }, []);
 
+  const shoppingLists = useSelector((store) => store.lists.shoppingLists);
+  const todoLists = useSelector((store) => store.lists.todoLists);
+
+  const loadRightText = () => {
+    if (shoppingLists.length === 0 && todoLists.length === 0) {
+      return (
+        <h1 style={{ color: "#ddd6f3", textShadow: " 0 0 20px #ddd6f3" }}>
+          Create a list to start
+        </h1>
+      );
+    } else {
+      return (
+        <h1
+          style={{
+            color: "#ddd6f3",
+            textShadow: "0 0 20px rgba(221,214,243,0.7)",
+          }}
+        >
+          Select a list to start
+        </h1>
+      );
+    }
+  };
+
   return (
     <div className="profile-container">
       <Header />
-      <div style={{ display: "flex", flexDirection: "row", width: "100vw" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100vw",
+          maxHeight: "100vh",
+        }}
+      >
         <span style={{ width: "25vw" }}>
           <MenuLists />
+        </span>
+        <span
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            flexDirection: "column",
+          }}
+        >
+          <Image style={{ width: "25vw" }} />
+          {loadRightText()}
         </span>
       </div>
     </div>
