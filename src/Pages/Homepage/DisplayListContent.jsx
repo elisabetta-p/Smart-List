12ab819a675a2a4e0a681d5eec539c3a3fc3a470
Elisabetta <elisabetta.piombin@gmail.com>
2020-08-09 21:57:06 +0200

    edit and delete single item buttons

--- src/Pages/Homepage/DisplayListContent.jsx
+++ src/Pages/Homepage/DisplayListContent.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {} from "../../Redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { HeartTwoTone } from "@ant-design/icons";
 
 const DisplayListContent = (props) => {
@@ -39,18 +38,26 @@ const DisplayListContent = (props) => {
   const loadTheItems = (id) => {
     return props.list.items.map((item, index) =>
       item.category === id ? (
-        <li key={index} className="single-item-in-list">
-          <span
-            style={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <input type="checkbox" className="inside-li" />{" "}
-            <span className="inside-li">{item.name}</span>
-            {item.isFav ? <HeartTwoTone twoToneColor="#faaca8" /> : null}
-          </span>
-        </li>
+        <tr key={index} className="single-item-in-list">
+          <td style={{ width: "1rem" }}>
+            <input type="checkbox" className="inside-td" />
+          </td>
+          <td>
+            {" "}
+            <span className="inside-td">{item.name}</span>
+          </td>
+          <td>{item.isFav ? <HeartTwoTone twoToneColor="#faaca8" /> : null}</td>
+          <td style={{ width: "4rem" }}>
+            <button className="button button-list button-inside-td">
+              Edit item
+            </button>
+          </td>
+          <td style={{ width: "4rem" }}>
+            <button className="button button-list button-inside-td delete-button">
+              Delete item
+            </button>
+          </td>
+        </tr>
       ) : null
     );
   };
@@ -61,16 +68,18 @@ const DisplayListContent = (props) => {
 
   return (
     <div id="display-list">
-      {categoryNames.map((cat) => (
-        <ul>
-          <h2 className="list-name-homepage category-name-display">
-            {cat.name}
-          </h2>
-          <span className="scroll-items list-items">
+      <table>
+        {categoryNames.map((cat) => (
+          <React.Fragment>
+            <thead>
+              <span className="list-name-homepage category-name-display">
+                {cat.name}
+              </span>
+            </thead>
             {loadTheItems(cat.id)}
-          </span>
-        </ul>
-      ))}
+          </React.Fragment>
+        ))}
+      </table>
     </div>
   );
 };
