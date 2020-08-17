72b59df37c9cd1243bbab3f820b71661c6d56b86
elisabetta-p <elisabetta-p@users.noreply.github.com>
2020-08-17 14:22:44 +0000

    deploy: 8a55f23f6af3a90f387a200b83427e4152435ff5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Smart-List/precache-manifest.b9e25d043e86d00ef58ce1c644fd3635.js"
+  "/Smart-List/precache-manifest.eba08846d9b678259a68d4d55d64c3d2.js"
 );
 
 self.addEventListener('message', (event) => {
