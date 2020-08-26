27600524d51e7a781484e13113f854cec98b51a3
elisabetta-p <elisabetta-p@users.noreply.github.com>
2020-08-26 14:03:43 +0000

    deploy: 48890fe9454f7c54a0f474b8c1203e089172e816

---
+++ precache-manifest.b9418833b8f45fb585b2a2c7ba4c6468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea89b0b37a909ac0082eec08f7bb297",
+    "url": "/Smart-List/index.html"
+  },
+  {
+    "revision": "fb35c6b6a9083854d525",
+    "url": "/Smart-List/static/css/2.8b3720a7.chunk.css"
+  },
+  {
+    "revision": "01d6407fd670545ef5b0",
+    "url": "/Smart-List/static/css/main.8b10e3d9.chunk.css"
+  },
+  {
+    "revision": "fb35c6b6a9083854d525",
+    "url": "/Smart-List/static/js/2.861590cf.chunk.js"
+  },
+  {
+    "revision": "58c520c716524d80673637b103bae188",
+    "url": "/Smart-List/static/js/2.861590cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d6407fd670545ef5b0",
+    "url": "/Smart-List/static/js/main.3fb599ad.chunk.js"
+  },
+  {
+    "revision": "596fa89958bcbb9c2ae0",
+    "url": "/Smart-List/static/js/runtime-main.293aa54d.js"
+  }
+]);
